c21251b78fa42b7cac9bb92492b3d3d795b3c060
Anacxx <anacarolina.clcx@gmail.com>
2023-06-28 14:35:55 -0300

    dia3

--- src/database.ts
+++ src/database.ts
@@ -1,34 +1,62 @@
 import { Tuser } from "./types"
 import { Tproduct } from "./types"
 
-export const users:Tuser[] = [
-{
-    id: '001',
-    name: 'Ana',
-    email: 'ana@email.com',
-    password: 'blabla123',
-    createdAt: new Date().toISOString(),
-},{
-    id: '002',
-    name: 'Emily',
-    email: 'emily@email.com',
-    password: 'bla123',
-    createdAt: new Date().toISOString(),
-}
-]
-
-export const products:Tproduct[] = [
+    export const users:Tuser[] = [
     {
-        id: '0034',
-        name: 'Bala',
-        price: 0.5,
-        description: 'Bala de café',
-        imageUrl: 'https...',
+        id: '001',
+        name: 'Ana',
+        email: 'ana@email.com',
+        password: 'blabla123',
+        createdAt: new Date().toISOString(),
     },{
-        id: '0036',
-        name: 'Chocolate',
-        price: 6.00,
-        description: 'Chocolate 70% de cacau',
-        imageUrl: 'https..',
+        id: '002',
+        name: 'Emily',
+        email: 'emily@email.com',
+        password: 'bla123',
+        createdAt: new Date().toISOString(),
     }
     ]
+    export function createUser(id: string, name: string, email: string, password: string): string {
+        const createdAt = new Date().toISOString();
+        const newUser: Tuser = { id, name, email, password, createdAt };
+        users.push(newUser);
+        return "Cadastro realizado com sucesso";
+    }
+
+    export function getAllUsers(): Tuser[] {
+        return users;
+    }
+
+    export const products:Tproduct[] = [
+        {
+            id: '0034',
+            name: 'Bala',
+            price: 0.5,
+            description: 'Bala de café',
+            imageUrl: 'https...',
+        },{
+            id: '0036',
+            name: 'Chocolate',
+            price: 6.00,
+            description: 'Chocolate 70% de cacau',
+            imageUrl: 'https..',
+        }
+        ]
+    export function createProduct(id:string, name:string, price:number, description:string,imageUrl:string){
+        const newProduct: Tproduct = { id, name, price, description, imageUrl };
+        products.push(newProduct)
+        return 'Produto criado com sucesso!'
+    }
+    export function getAllProducts():Tproduct[]{
+        return products
+    }
+
+    export function searchProductsByName(name:string):Tproduct[] {
+        const searchTerm = name.toLowerCase();
+        const matchingProducts = products.filter((product) => {
+            const productName = product.name.toLowerCase();
+            return productName.includes(searchTerm);
+        });
+        
+        return matchingProducts;
+        }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { users } from "./database";
-import { products } from "./database";
-console.log('O aplicativo foi iniciado,tudo certo.')
-console.log(users)
-console.log(products)
+import { getAllProducts, getAllUsers, users } from "./database";
+import { createUser } from "./database";
+import { createProduct } from "./database";
+import { searchProductsByName } from "./database";
+// console.log('O aplicativo foi iniciado,tudo certo.')
+// console.log(createUser('001','Ana Carolina', 'ana@gmail.com', '123443'))
+// console.log(getAllUsers())
+console.log(createProduct('0323','Acido Retinóico',40.00,'Ácido para tirar a oleosidade','http...'))
+console.log('parte2')
+console.log(searchProductsByName('Chocolate'))
+console.log('parte3')
+console.log(getAllProducts())
